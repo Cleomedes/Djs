acb7ce39a864dd306ab161554e7d9451dc04f478
Cleomedes <antwauntune25@wcpsstudent.org>
2016-05-05 07:54:35 -0400

    Update d.js

--- d.js
+++ d.js
@@ -1,41 +1,81 @@
-  function D(selector) {
-        if (!(this instanceof D)) {
-            return new D(selector);
-        }
-        if (!selector) {
-            return this;
-        }
-        if (selector instanceof D) {
-            return selector;
-        }
-        if (selector.nodeType) {
-            this[0] = selector;
-            this.length = 1;
-            return this;
-        }
-        if (typeof selector === 'string') {
-            return push.apply(this, slice.call(document.querySelectorAll(selector)));
-        }
-        if (typeof selector === 'function') {
-            return D(document).ready(selector);
-        }
+'use strict';
+var _mainLoaded = false;
+
+function D(selector) {
+    if (!(this instanceof D)) {
+        return new D(selector);
     }
-    var Error = {
-        session: function() {
-            console.log("Could not establish a connection session.");
-            alert("Could not establish a connection session.");
-        }
-    };
-    D.prototype = {
-        length: 0,
-        ready: function(callback) {
-            if (/t/.test(document.readyState)) {
+    if (!selector) {
+        return this;
+    }
+    if (selector instanceof D) {
+        return selector;
+    }
+    if (selector.nodeType) {
+        this[0] = selector;
+        this.length = 1;
+        return this;
+    }
+    if (typeof selector === 'string') {
+        return push.apply(this, slice.call(document.querySelectorAll(selector)));
+    }
+    if (typeof selector === 'function') {
+        return D(document).ready(selector);
+    }
+}
+D.prototype = {
+    length: 0,
+    Main: function(callback) {
+        _mainLoaded = true;
+        if (/t/.test(document.readyState)) {
+            callback(D);
+        } else {
+            document.addEventListener('DOMContentLoaded', function() {
                 callback(D);
-            } else {
-                document.addEventListener('DOMContentLoaded', function() {
-                    callback(D);
-                }, false);
-            }
+            }, false);
         }
     }
-    
+
+}
+var D = D.prototype;
+
+function int(name, integer) {
+    if (_mainLoaded == true) {
+        if (typeof integer == 'number') {
+            window[name] = integer;
+        } else {
+            throw new Error("Invalid type for integer.")
+        }
+    } else {
+        throw new Error("No main method.")
+    }
+}
+
+function string(name, string) {
+    if (_mainLoaded == true) {
+        if (typeof string == 'string') {
+            window[name] = integer;
+        } else {
+            throw new Error("Invalid type for string.")
+        }
+    } else {
+        throw new Error("No main method.")
+    }
+}
+
+function char(name, char) {
+    if (_mainLoaded == true) {
+        if (typeof string == 'string') {
+            if(char.length == 1){
+            window[name] = char.toUpperCase();
+            } else {
+                throw new Error("Invalid length for char")
+            }
+        } else {
+            throw new Error("Invalid type for string.")
+        }
+    } else {
+        throw new Error("No main method.")
+    }
+}
+
